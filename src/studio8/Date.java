652ed7bce7e913b575fed9d924a552f3cec82350
fareeha <fareeha@suvismac.dhcp.wustl.edu>
2022-11-17 14:12:46 -0600

    studio 8

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,81 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
 
+	private String month;
+	private int day;
+	private int year;
+	private boolean isHoliday;
 
-    public static void main(String[] args) {
+	
+	/**
+	 * @param month    the month that this date is in.
+	 * @param day      the day (1-31) of the month.
+	 * @param year     the year that this date is in.
+	 * @param isHoliday whether or not this day is a holiday.
+	 *                 true if holiday, false otherwise.
+	 */
+	public Date (String month, int day, int year, boolean isHoliday) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.isHoliday = isHoliday;
+	}
+	
+	public String toString() {
+		String result =  month + " " + day + ", " + year;
+		if (isHoliday) {
+			result = result + "\nThis is a holiday!";
+		} else {
+			result = result + " \nThis is not a holiday.";
+		}
+		return result;
+	}
+	
+    @Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && Objects.equals(month, other.month) && year == other.year;
+	}
+
+	public static void main(String[] args) {
+    	
+    	Date d1=new Date("December",25,2008,true);
+    	System.out.println(d1);
+    	Date d2 = new Date("December", 25, 2008, false);
+    	Date d3 = new Date("January", 3, 2006, false);
+    	Date d4 = new Date("March", 21, 2016, true);
+    	Date d5 = new Date("February", 5, 2011, false);
+    	System.out.println(d1.equals(d2));
+    	LinkedList<Date> list  = new LinkedList<Date>();
+		list.add(d1);
+		list.add(d2);
+		list.add(d3);
+		list.add(d4);
+		list.add(d5);
+		list.add(d5);
+		System.out.println(list);
+		
+		HashSet<Date> set = new HashSet<Date>();
+		set.add(d1);
+		set.add(d5);
+		set.add(d1);
+		System.out.println(set);
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,80 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
 
 public class Time {
 
+	private int hour;
+	private int minute;
+	private boolean is24Hr;
+
+	/**
+	 * @param hour     the hour.
+	 * @param minute   the minute of the time.
+	 * @param is24Hr   whether or not the time is shown in 24 hr format.
+	 *                 true if 24 hr, false is 12 hr.
+	 */
+	public Time(int hour, int minute, boolean is24Hr) {
+		this.hour = hour;
+		this.minute = minute;
+		this.is24Hr = is24Hr;
+	}
+
+	public String toString() {
+		String result = "";
+		if (is24Hr == true) {
+			result = hour + ":" + minute;
+		}	else if (is24Hr == false && hour > 12) {
+			result = hour - 12 + ":" + minute + " PM";
+		} else if (is24Hr == false && hour <= 12){
+			result = hour + ":" + minute + " AM";
+		}
+		return result;
+	}
+
 	public static void main(String[] args) {
- 
-    	
-    }
+  
+		Time t1= new Time(13,27,false);
+		System.out.println(t1);
+		
+		Time t2 = new Time(23, 11, true);
+		Time t3 = new Time(11, 11, false);
+		Time t4 = new Time(12,16,false);
+		Time t5 = new Time(14,27,true);
+		System.out.println(t1.equals(t2));
+		LinkedList<Time> list  = new LinkedList<Time>();
+		list.add(t1);
+		list.add(t2);
+		list.add(t3);
+		list.add(t4);
+		list.add(t5);
+		list.add(t5);
+		System.out.println(list);
+		
+		HashSet<Time> set = new HashSet<Time>();
+		set.add(t1);
+		set.add(t2);
+		set.add(t1);
+		System.out.println(set);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
 
 }
